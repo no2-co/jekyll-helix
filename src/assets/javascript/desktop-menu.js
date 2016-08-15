3662bd428b9b2107010027c6c4b1432a015c1701
Miguel Palau <mpalauzarza@gmail.com>
2016-08-14 23:25:07 -0500

    desktop general UX done

--- src/assets/javascript/desktop-menu.js
+++ src/assets/javascript/desktop-menu.js
@@ -11,4 +11,22 @@
     desktopDropdown.removeClass('is-active')
   })
 
+  // Make the search appear on hover
+  var searchToggle = $('.desktop-menu__right-side')
+  var searchDropdown = $('.desktop-search')
+
+  var searchInput = $('.desktop-search__search-input')
+
+  searchToggle.on('mouseover', function() {
+    searchDropdown.addClass('is-active')
+    searchInput.focus()
+  })
+
+  searchDropdown.on('mouseleave', function() {
+    searchDropdown.removeClass('is-active')
+    searchInput.blur()
+  })
+
+
+
 })(jQuery);
