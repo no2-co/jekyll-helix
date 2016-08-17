c23033e2091828b9f99ef54d3547ad9a10a42131
Miguel Palau <mpalauzarza@gmail.com>
2016-08-17 04:55:25 -0500

    added accordion to sidebar catalog

--- src/assets/javascript/catalog-sidebar.js
+++ src/assets/javascript/catalog-sidebar.js
@@ -7,4 +7,22 @@
     sidebar.toggleClass('is-active')
     body.toggleClass('js-catalog-sidebar-active')
   })
+
+  // Make accordion stuff
+  var toggle = $('.catalog-sidebar-filter .js-toggle')
+  toggle.on('click', function() {
+    var that = $(this)
+    var daddy = that.parents('.catalog-sidebar-filter')
+    var info = daddy.children('.catalog-sidebar-filter__options')
+
+    if ( daddy.hasClass('js-is-active') ){
+      info.slideUp()
+      daddy.removeClass('js-is-active')
+    } else {
+      info.slideDown()
+      daddy.addClass('js-is-active')
+    }
+
+
+  })
 })(jQuery);
