a521ef4c80657c0a6b1da2f3d4e44b8846c97ca6
Miguel Palau <mpalauzarza@gmail.com>
2016-08-17 03:25:25 -0500

    better fixed behaviour filters catalog mobile

--- src/assets/javascript/catalog-sidebar.js
+++ src/assets/javascript/catalog-sidebar.js
@@ -1,8 +1,10 @@
 ;(function($){
   var filtersToggle = $('.catalog-top-mobile__filters-toggle')
   var sidebar = $('.catalog-sidebar')
+  var body = $('body')
 
   filtersToggle.on('click', function() {
     sidebar.toggleClass('is-active')
+    body.toggleClass('js-catalog-sidebar-active')
   })
 })(jQuery);
