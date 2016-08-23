7bf6bb1eb641181876c9d0aa41bb535e2d5fb130
Miguel Palau <mpalauzarza@gmail.com>
2016-08-23 14:05:10 -0500

    fixed search and categories bug

--- src/assets/javascript/desktop-menu.js
+++ src/assets/javascript/desktop-menu.js
@@ -2,9 +2,16 @@
   // Make the dropdown appear on hover
   var blackBarItems = $('.desktop-menu__left-side a')
   var desktopDropdown = $('.desktop-dropdown')
+  // Make the search appear on hover
+  var searchToggle = $('.desktop-menu__right-side')
+  var searchDropdown = $('.desktop-search')
+
+  var searchInput = $('.desktop-search__search-input')
+
 
   blackBarItems.on('mouseover', function() {
     desktopDropdown.addClass('is-active')
+    searchDropdown.removeClass('is-active')
   })
 
   desktopDropdown.on('mouseleave', function() {
@@ -12,12 +19,8 @@
   })
 
   // Make the search appear on hover
-  var searchToggle = $('.desktop-menu__right-side')
-  var searchDropdown = $('.desktop-search')
-
-  var searchInput = $('.desktop-search__search-input')
-
   searchToggle.on('mouseover', function() {
+    desktopDropdown.removeClass('is-active')
     searchDropdown.addClass('is-active')
     searchInput.focus()
   })
