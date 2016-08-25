32f4d316a3f9da1260b7dfceb2b5ae4a2d3cc8f1
Miguel Palau <mpalauzarza@gmail.com>
2016-08-25 12:59:44 -0500

    more js advances product page

--- src/assets/javascript/product-overlay.js
+++ src/assets/javascript/product-overlay.js
@@ -3,9 +3,10 @@
   var body = $('body')
   var overlay = $('.product-overlay')
   var background = $('.page-content__overlay')
-  var select = $('.product-overlay__select')
+  var options = $('.product-overlay__select option')
   var deskOptions = $('.product-overlay__desk-options h2')
-  var section = $('.product-overlay__section')
+  var measureSection = $('.product-overlay__section--measurements')
+  var sizesSection = $('.product-overlay__section--sizeguide')
   var measureImage = $('.product-overlay__image img')
   var sizesRadios = $('.product-overlay__sizes .option-radio')
   var measureRadios = $('.product-overlay__sizes--measures .option-radio')
@@ -16,6 +17,10 @@
   triggerButton.on('click', function() {
     body.addClass('js-product-overlay-active')
     $("html, body").animate({ scrollTop: 0 }, "slow")
+
+    // Also make the modal be on the sizes thing
+    sizesSection.addClass('is-active')
+    measureSection.removeClass('is-active')
   })
 
   // Close the modal
